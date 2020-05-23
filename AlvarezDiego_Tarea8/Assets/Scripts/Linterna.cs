6add2f8f9d35b13072d6456f5cc7ea259a202f3d
diego59x <diegoalvarez1237@hotmail.com>
2020-05-23 13:48:24 -0600

    juego listo

---
+++ AlvarezDiego_Tarea8/Assets/Scripts/Linterna.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Linterna : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Light luz;
+    private AudioSource on;
+    public Text scoreText;
+    void Start()
+    {
+        on = GetComponent<AudioSource>();
+        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+        luz = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { //aca puedes cambiar la letra por la que quieras que
+          //prenda y apague la luz
+
+            if (luz.enabled == true)
+            {
+               
+
+                luz.enabled = false;
+
+            }
+            else
+            {
+                on.Play();
+                luz.enabled = true;
+            }
+
+        }
+        Ray myRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        RaycastHit hitInfo;
+        if (Physics.Raycast(myRay, out hitInfo))
+        {
+            if (hitInfo.collider.CompareTag("Cube"))
+            {
+
+                scoreText.text = "That is a: " + hitInfo.collider.ToString();
+                
+            }
+            else
+            {
+                scoreText.text = " ";
+            }
+
+        }
+
+
+
+    }
+
+}
---
+++ AlvarezDiego_Tarea8/Assets/Scripts/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Light luz;
+    void Start()
+    {
+           
+    }
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        luz.enabled = true;
+        luz.intensity = Mathf.Abs(Mathf.Sin(Time.time) * 5);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        luz.enabled = false;
+    }
+}
